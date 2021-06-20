7900bf6d4713223ca10d8df348240b1c5d8ea607
ercimen <ercimen@hotmail.com>
2021-06-20 19:42:25 +0300

    Last Update
    ...

--- Assets/Scripts/Arrow_Control.cs
+++ Assets/Scripts/Arrow_Control.cs
@@ -7,11 +7,11 @@ public class Arrow_Control : MonoBehaviour
    
     public float inputX;
     [SerializeField] float ArrowSpeed;
-    Ragdoll_Manager ragdolls; // Ragdoll manager araciligi ile oyuncu aktif edecegiz
+    
 
     private void Awake()
     {
-        ragdolls = FindObjectOfType<Ragdoll_Manager>();
+        
     }
     void Update()
     {
@@ -25,7 +25,7 @@ public class Arrow_Control : MonoBehaviour
         Vector3 Direction = new Vector3(inputX, 0, 0);
         transform.Translate(Direction * Time.deltaTime * 5, Space.World);
 
-        if (Input.GetKeyDown(KeyCode.Space)) ragdolls.CheckPassiveObjects(1, this.transform);
+        if (Input.GetKeyDown(KeyCode.Space)) Ragdoll_Manager.Instance.CheckPassiveObjects(1, this.transform);
 
 
     }
