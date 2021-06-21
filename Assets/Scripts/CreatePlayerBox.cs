0e0494d48ec4501b6503afa70eab281288425dcd
Atakan Çakır <atakan_naci@hotmail.com>
2021-06-21 14:06:26 +0300

    effect

--- Assets/Scripts/CreatePlayerBox.cs
+++ Assets/Scripts/CreatePlayerBox.cs
@@ -5,17 +5,9 @@ using UnityEngine.UI;
 public class CreatePlayerBox : MonoBehaviour
 {
     [SerializeField] byte PlayerCount;
-    [SerializeField] Text txt;
-   
-    void Start()
-    {
-      
-      //  txt.GetComponent<UnityEngine.UI.Text>().text = PlayerCount.ToString();
-    }
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Enemy") Enemy_Manager.Instance.CreateBoX(PlayerCount, this.transform.position);
-        if (other.tag == "Player") Ragdoll_Manager.Instance.CreateBoX(PlayerCount, this.transform.position);
-
+        if (other.tag == "Enemy") Enemy_Manager.Instance.CreateBoX(PlayerCount, this.transform.position+new Vector3(0,0,-1f));
+        if (other.tag == "Player") Ragdoll_Manager.Instance.CreateBoX(PlayerCount, this.transform.position+new Vector3(0,0,2f));
     }
 }
